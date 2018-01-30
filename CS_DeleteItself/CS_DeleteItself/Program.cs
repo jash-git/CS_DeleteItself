beb9f3d5f8b627c790bb23671d5a1e1aa3e435b5
jash-git <jash.liao@gmail.com>
2018-01-30 09:48:55 +0800

    Create

---
+++ CS_DeleteItself/CS_DeleteItself/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+//http://www.cnblogs.com/Interkey/p/DeleteItself.html
+namespace CS_DeleteItself
+{
+    class Program
+    {
+
+        private static void DeleteItselfByCMD()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("cmd.exe", "/C ping 1.1.1.1 -n 1 -w 1000 > Nul & Del " + System.AppDomain.CurrentDomain.BaseDirectory + "\\" + System.AppDomain.CurrentDomain.FriendlyName);//WINFORM 路徑 Application.ExecutablePath
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            psi.CreateNoWindow = true;
+            Process.Start(psi);
+            //Application.Exit();//WINFORM 自行結束自己 
+        } 
+        static void pause()
+        {
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(System.AppDomain.CurrentDomain.BaseDirectory + "\\" + System.AppDomain.CurrentDomain.FriendlyName);
+            pause();
+            DeleteItselfByCMD();
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,5 @@
+﻿C#删除程序自身(自殺小程式)
+
+參考資料:http://www.cnblogs.com/Interkey/p/DeleteItself.html
+
+我把它改成可以支援CONSOLE模式，原本的WINFORM模式用註解保留在程式碼之中
